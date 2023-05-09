794fc4819bc7d1352381bbecc4d2b471fd665414
bjoern <bjoern.crombach95@gmail.com>
2023-05-09 21:43:47 +0200

    feat: add four new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectComponent } from './select/select.component';
+import { CraftComponent } from './craft/craft.component';
+import { PriceComponent } from './price/price.component';
+import { CalcComponent } from './calc/calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent,
+    CraftComponent,
+    PriceComponent,
+    CalcComponent
   ],
   imports: [
     BrowserModule,
